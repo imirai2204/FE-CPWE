e9b359a8ba3fde19ca3e2b120d449b921c837048
Tung Huynh <tunghttcs20007@fpt.edu.vn>
2022-03-06 03:48:01 +0700

    Implement MUI-Popper to display User Card Info

--- src/components/Navbar/greeting/User.js
+++ src/components/Navbar/greeting/User.js
@@ -1,27 +1,55 @@
-import React, { useState, Fragment } from "react";
-import Button from "@mui/material/Button";
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import Popper from "@mui/material/Popper";
+import Fade from "@mui/material/Fade";
 import Greeting from "./Greeting";
 import Avatar from "@mui/material/Avatar";
-import UserCard from "./UserCard";
+import Button from "@mui/material/Button";
 
 const defaultAvatar = "/default-avatar.png";
 
-export default function BasicMenu(props) {
-    const [showMenu, setShowMenu] = useState(false);
+const User = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [toggleDisplay, setToggleDisplay] = useState(null);
 
-    const userCardHandler = () => {
-        setShowMenu((prev) => !prev);
+    const handleClick = (event) => {
+        setToggleDisplay(event.currentTarget);
+        setIsOpen((previousOpen) => !previousOpen);
     };
 
     const imgSourcePath = props.src !== undefined ? props.src : defaultAvatar;
 
+    const canBeOpen = isOpen && Boolean(toggleDisplay);
+    const id = canBeOpen ? "transition-popper" : undefined;
+
     return (
-        <Fragment>
-            <Button id='basic-button' onClick={userCardHandler}>
+        <nav>
+            <Button
+                id='basic-button'
+                aria-describedby={id}
+                type='button'
+                onClick={handleClick}>
                 <Greeting data={props.data} />
                 <Avatar id='user-avatar-navbar' src={imgSourcePath} />
             </Button>
-            {showMenu && <UserCard onClose={userCardHandler} imgSource={imgSourcePath} />}
-        </Fragment>
+            <Popper id={id} open={isOpen} anchorEl={toggleDisplay} transition>
+                {({ TransitionProps }) => (
+                    <Fade {...TransitionProps} timeout={250}>
+                        <Box
+                            sx={{
+                                width: 280,
+                                height: 300,
+                                border: 0,
+                                borderRadius: 2,
+                                boxShadow: 4,
+                                p: 0,
+                                bgcolor: "background.paper",
+                            }}></Box>
+                    </Fade>
+                )}
+            </Popper>
+        </nav>
     );
-}
+};
+
+export default User;
