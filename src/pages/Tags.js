d390c8acee3a0d66935ab8f4eb8725fc4a9e8040
Tung Huynh <tunghttcs20007@fpt.edu.vn>
2022-03-11 04:30:02 +0700

    [CPWE-33] Developing tags page

--- src/pages/Tags.js
+++ src/pages/Tags.js
@@ -1,7 +1,115 @@
-import React from "react";
+import React, { useContext } from "react";
+import "../styles/style.scss";
+import { ErrorMessage, Formik, Form } from "formik";
+import { TextField } from "../components/UI/Form/TextField";
+import { EnhancedTable } from "../components/UI/Table/Table";
+import { DepartmentSchema } from "../validation";
+import axios from "axios";
+import { TagsData, TableColumns, TopicOptions } from "./dummy-data/tags-page";
+import Select from "react-select";
 
-function Tags() {
-    return <h1>Tags</h1>;
+function handleSubmit(values) {
+    const body = {
+        departmentName: values.departmentName,
+    };
+
+    axios
+        .post(`http://localhost:3000/login`, body)
+        .then((res) => {
+            console.log(res);
+            console.log(res.data);
+            setTimeout(() => {
+                alert("Login success");
+            }, 400);
+        })
+        .catch((error) => {
+            console.log(error);
+            setTimeout(() => {
+                alert(error);
+            }, 400);
+        });
 }
+
+const initialValues = {
+    departmentName: "",
+};
+
+const Tags = (props) => {
+    return (
+        <div className='department-page container'>
+            <h2 className='page-title'>Tag</h2>
+            <div className='layout-form'>
+                <Formik
+                    initialValues={initialValues}
+                    validationSchema={DepartmentSchema}
+                    onSubmit={(values, { setSubmitting }) => {
+                        handleSubmit(values);
+                    }}>
+                    {({
+                        isSubmiting,
+                        handleChange,
+                        handleBlur,
+                        handleSubmit,
+                        values,
+                        errors,
+                        touched,
+                        setFieldValue,
+                    }) => (
+                        <Form className='submit-form'>
+                            <div className='form-container'>
+                                <div className='input-section label-mark'>
+                                    <TextField
+                                        label={"Tag"}
+                                        name='tag'
+                                        type='text'
+                                        placeholder='Tag name...'
+                                        // value={editTableCtx.inputFieldValue}
+                                        // onChange={handleChange}
+                                    />
+                                </div>
+                                <div className='input-section label-mark'>
+                                    <label className='label'>Topic</label>
+                                    <Select
+                                        className='select'
+                                        name='topic'
+                                        id='topic'
+                                        options={TopicOptions}
+                                        placeholder={"Select topic"}
+                                        onChange={(selectOption) => {
+                                            setFieldValue("topicId", selectOption.value);
+                                        }}
+                                        onBlur={() => {
+                                            handleBlur({ target: { name: "topic" } });
+                                        }}
+                                    />
+                                    <ErrorMessage
+                                        component='div'
+                                        name={"topicId"}
+                                        className='error'
+                                    />
+                                </div>
+                            </div>
+                            <hr />
+                            <div className='list-button'>
+                                <button className='btn btn-warning' type='submit'>
+                                    Search
+                                </button>
+                                <button className='btn btn-info' type='reset'>
+                                    Refresh
+                                </button>
+                                <button className='btn btn-success' type='submit'>
+                                    Save
+                                </button>
+                            </div>
+                        </Form>
+                    )}
+                </Formik>
+            </div>
+            <div className='layout-table'>
+                <EnhancedTable columns={TableColumns} rows={TagsData} />
+            </div>
+        </div>
+    );
+};
 
 export default Tags;
---
+++ src/pages/dummy-data/tags-page.js
@@ -0,0 +1,59 @@
+export const TagsData = [
+    {
+        id: 1,
+        department: "Information Technology Department",
+    },
+    {
+        id: 2,
+        department: "Human Resources Department",
+    },
+    {
+        id: 3,
+        department: "Economics Department",
+    },
+    {
+        id: 4,
+        department: "English and Comparative Literature Department",
+    },
+    {
+        id: 5,
+        department: "Mechanical Engineering Department",
+    },
+    {
+        id: 6,
+        department: "History Department",
+    },
+    {
+        id: 7,
+        department: "Mathematics Department",
+    },
+    {
+        id: 8,
+        department: "English and Comparative Literature Department",
+    },
+    {
+        id: 9,
+        department: "Mechanical Engineering Department",
+    },
+    {
+        id: 10,
+        department: "History Department",
+    },
+    {
+        id: 11,
+        department: "Mathematics Department",
+    },
+];
+
+export const TableColumns = [
+    { id: "id", label: "ID", width: "5%", align: "center", style: { left: "13px" } },
+    { id: "department", label: "Department", width: "50%", align: "left" },
+];
+
+export const TopicOptions = [
+    { value: 1, label: "Topic 1", key: "1" },
+    { value: 2, label: "Topic 2", key: "2" },
+    { value: 3, label: "Topic 3", key: "3" },
+    { value: 4, label: "Topic 3", key: "4" },
+    { value: 5, label: "Topic 3", key: "5" },
+];
