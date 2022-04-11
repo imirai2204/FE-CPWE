f7c7bd02dd97d80e12c84995221a987b31f7ab1a
Nguyen Anh Tu <62977426+NguyenAnhTu4869@users.noreply.github.com>
2022-04-11 12:26:48 +0700

    Update IdeaDetail.js
    Fix component comment in page idea detail

--- src/pages/IdeaDetail.js
+++ src/pages/IdeaDetail.js
@@ -12,68 +12,66 @@ function IdeaDetail() {
     const currentUserName = useSelector((state) => state.user.userInfo.fullName);
 
     return (
-        <Fragment>
-            <div className='container'>
-                <div className='idea-detail-page'>
-                    <h2 className='idea-title'>Idea Detail</h2>
-                    <div className='idea-body'>
-                        <div className='author'>
-                            <Avatar className='author-avatar' />
-                            <div className='author-info'>
-                                <label className='author-name'>
-                                    {isAnonymous ? "Anonymous" : "Test user"}
-                                </label>
-                                <p className='date-submit'>18/04/2022</p>
-                            </div>
-                        </div>
-                        <VisibilityIcon onClick={() => setIsAnonymous(!isAnonymous)} />
-                        <div className='idea-description'>
-                            <p>
-                                In descriptive writing, the author does not just tell the
-                                reader what was seen, felt, tested, smelled, or heard.
-                                Rather, the author describes something from their own
-                                experience and, through careful choice of words and
-                                phrasing, makes it seem real. Descriptive writing is
-                                vivid, colorful, and detailed.
-                            </p>
-                        </div>
-                        <div className='idea-document'>
-                            <label>List upload document</label>
-                            <ul>
-                                <li>
-                                    <Link
-                                        to='/terms-conditions'
-                                        className='link-item'
-                                        target='_blank'>
-                                        test.docx
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link
-                                        to='/terms-conditions'
-                                        className='link-item'
-                                        target='_blank'>
-                                        test.pdf
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link
-                                        to='/terms-conditions'
-                                        className='link-item'
-                                        target='_blank'>
-                                        test.xlsx
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                        <div className='idea-image'>
-                            <label>List upload image</label>
+        <div className='container'>
+            <div className='idea-detail-page'>
+                <h2 className='idea-title'>Idea Detail</h2>
+                <div className='idea-body'>
+                    <div className='author'>
+                        <Avatar className='author-avatar' />
+                        <div className='author-info'>
+                            <label className='author-name'>
+                                {isAnonymous ? "Anonymous" : "Test user"}
+                            </label>
+                            <p className='date-submit'>18/04/2022</p>
                         </div>
                     </div>
+                    <VisibilityIcon onClick={() => setIsAnonymous(!isAnonymous)} />
+                    <div className='idea-description'>
+                        <p>
+                            In descriptive writing, the author does not just tell the
+                            reader what was seen, felt, tested, smelled, or heard.
+                            Rather, the author describes something from their own
+                            experience and, through careful choice of words and
+                            phrasing, makes it seem real. Descriptive writing is
+                            vivid, colorful, and detailed.
+                        </p>
+                    </div>
+                    <div className='idea-document'>
+                        <label>List upload document</label>
+                        <ul>
+                            <li>
+                                <Link
+                                    to='/terms-conditions'
+                                    className='link-item'
+                                    target='_blank'>
+                                    test.docx
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    to='/terms-conditions'
+                                    className='link-item'
+                                    target='_blank'>
+                                    test.pdf
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    to='/terms-conditions'
+                                    className='link-item'
+                                    target='_blank'>
+                                    test.xlsx
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className='idea-image'>
+                        <label>List upload image</label>
+                    </div>
+                    <Comments currentUserId={currentUserId} currentUserName={currentUserName} />
                 </div>
             </div>
-            <Comments currentUserId={currentUserId} currentUserName={currentUserName} />
-        </Fragment>
+        </div>
     );
 }
 
