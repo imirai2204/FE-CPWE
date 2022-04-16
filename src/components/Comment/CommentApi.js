6e370336e80eb55d0e038b09f8751b9156d80712
tunghttcs20007 <tunghttcs20007@fpt.edu.vn>
2022-04-16 22:54:44 +0700

    Integrate Comments component with BE

--- src/components/Comment/CommentApi.js
+++ src/components/Comment/CommentApi.js
@@ -1,41 +1,67 @@
 import { AxiosInstance } from "../../api/AxiosClient";
 import { IdeaUrl } from "../../api/EndPoint";
 
+const errorMessage = (error) => {
+    if (error && error.response) {
+        console.log("Error: ", error);
+    }
+};
+
 export const createIdeaComment = async (body) => {
+    let newComment = {
+        id: null,
+        content: "",
+        username: "",
+        userId: "",
+        parent: null,
+        createdAt: "",
+    };
+
     await AxiosInstance.post(IdeaUrl.comment, body, {
         headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
     })
         .then((res) => {
-            var errorData = {
-                code: res.data.code,
-                message: res.data.message,
+            console.log(body);
+            const responseData = res.data.data;
+            newComment = {
+                id: `${responseData.id}`,
+                content: responseData.content,
+                userName: responseData.createdUser,
+                userId: responseData.userId,
+                parent: responseData.parent !== null ? `${responseData.parent}` : null,
+                createdAt: responseData.createdDate,
             };
-            console.log("Create success");
         })
         .catch((error) => {
-            if (error && error.response) {
-                console.log("Error: ", error);
-            }
+            errorMessage(error);
         });
+    console.log(newComment);
+    return newComment;
 };
 
 export const fetchIdeaComments = async (ideaId) => {
-    let responseData = [];
+    let allComments = [];
+    let responseData;
     await AxiosInstance.get(IdeaUrl.fetchComment.concat(`/${ideaId}`), {
         headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
     })
         .then((res) => {
             responseData = res.data.data;
-            var errorData = {
-                code: res.data.code,
-                message: res.data.message,
-            };
+            console.log(responseData);
             console.log("Fetch Comment Success");
         })
         .catch((error) => {
-            if (error && error.response) {
-                console.log("Error: ", error);
-            }
+            errorMessage(error);
         });
-    return responseData;
+    responseData.forEach((data) => {
+        allComments.push({
+            id: `${data.id}`,
+            content: data.content,
+            userName: data.createdUser,
+            userId: data.userId,
+            parent: data.parent !== null ? `${data.parent}` : null,
+            createdAt: data.createdDate,
+        });
+    });
+    return allComments;
 };
