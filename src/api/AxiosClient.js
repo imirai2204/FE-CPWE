9fc77485cd0857ba88e5752fd95407d22fe9b941
longnpggcs <longnpgcs18266@fpt.edu.vn>
2022-04-18 08:35:13 +0700

    Update AxiosClient.js

--- src/api/AxiosClient.js
+++ src/api/AxiosClient.js
@@ -4,7 +4,7 @@ import axios from "axios";
 // const BASE_URL = "http://cpwe-test.mocklab.io";
 /*************************************************************************/
 
-const BASE_URL = "http://b3b0-42-112-228-184.ngrok.io";
+const BASE_URL = "http://19fd-42-112-228-184.ngrok.io";
 
 const defaultConfig = {
     baseURL: BASE_URL,
